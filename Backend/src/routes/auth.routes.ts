f524a196bd1b9c07fef181e5bf5f72c46d7d1f06
Vincze Norbert-Istvan (CljP/HRL-PJ) <vin3clj@bosch.com>
2026-08-05 09:58:09 +0300

    auth rate limiter

--- Backend/src/routes/auth.routes.ts
+++ Backend/src/routes/auth.routes.ts
@@ -2,12 +2,17 @@ import { Router } from "express";
 import { AuthController } from "../controllers/AuthController.ts";
 import { authMiddleware } from "../middleware/auth.middleware.ts";
 import { adminMiddleware } from "../middleware/admin.middleware.ts";
+import { authRateLimiter } from "../middleware/rateLimit.middleware.ts";
 
 export const authRouter = Router();
 
-authRouter.post("/register", (req, res) => AuthController.register(req, res));
+authRouter.post("/register", authRateLimiter, (req, res) =>
+  AuthController.register(req, res),
+);
 
-authRouter.post("/login", (req, res) => AuthController.login(req, res));
+authRouter.post("/login", authRateLimiter, (req, res) =>
+  AuthController.login(req, res),
+);
 
 authRouter.post("/reset-password", (req, res) =>
   AuthController.resetPassword(req, res),
